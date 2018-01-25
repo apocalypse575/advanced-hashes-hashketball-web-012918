4c354959b67ff891380e82c0cbc2120a06e33ce1
apocalypse575 <apocalypse575@gmail.com>
2018-01-25 01:13:15 +0000

    automatically backed up by learn

--- hashketball.rb
+++ hashketball.rb
@@ -154,7 +154,7 @@ def num_points_scored(name)
               # puts stats[:points]
 
           if player == name
-              return 10
+              puts stats[:points]
               # stats[:points]
           end
         end
